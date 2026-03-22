dc8b0280483cce89c1de4a8140231c6fa9561a5a
Anastasia <stasiaevsina@gmail.com>
2026-03-22 23:00:20 +0700

    chore: add allowedDevOrigins for voicezettel.ru

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
+    allowedDevOrigins: ["https://voicezettel.ru", "http://voicezettel.ru"],
     devIndicators: false,
     turbopack: {},
     images: {
